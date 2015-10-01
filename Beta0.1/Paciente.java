ba1af3a6927deecd4d804fd15e66eb91f33e5c58
mor14012 <diegomorales@MacBook-Air-de-Diego.local>
2015-10-01 08:05:12 -0600

    Clase paciente finalizada

--- Beta0.1/Paciente.java
+++ Beta0.1/Paciente.java
@@ -1,6 +1,27 @@
-public class Paciente implements Comparable<E> {
+public class Paciente implements Comparable<Paciente> {
+	private String string;
+	private int value;
 
-	public int compareTo(E object){
-		System.out.println("compareTo");
+	public void setstring(String string){
+		this.string = string;
+	}
+	public String getstring(){
+		return string;
+	}
+	public void setvalue(int value){
+		this.value = value;
+	}
+	public int getvalue(){
+		return value;
+	}
+	public int compareTo(Paciente paciente){
+		if(value - paciente.getvalue()>0)
+			return 1;
+		if(value - paciente.getvalue()<0)
+			return -1;
+		return 0;
+	}
+	public String toString(){
+		return "String: "+string+"\nValue: "+value;
 	}
 }
