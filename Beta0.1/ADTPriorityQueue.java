802175121ad03603801f90ff1da147ea5321d7ea
mor14012 <diegomorales@MacBook-Air-de-Diego.local>
2015-10-01 07:36:19 -0600

    ADTPriorityQueue

---
+++ Beta0.1/ADTPriorityQueue.java
@@ -0,0 +1,13 @@
+public interface ADTPriorityQueue<E extends Comparable<E>>{
+	public E getFirst();
+
+	public E remove();
+
+	public void add(E value);
+
+	public boolean isEmpty();
+
+	public int size();
+
+	public void clear();
+}
---
+++ Beta0.1/VectorHeap.java
@@ -0,0 +1,5 @@
+import java.util.PriorityQueue;
+
+class VectorHeap<E extends Comparable<E>> implements PriorityQueue<E>{
+	
+}
