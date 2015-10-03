2e31e479bbe8ac668d261a09d6b5ebe8f8a596b3
mor14012 <diegomorales@MacBook-Air-de-Diego.local>
2015-10-03 12:20:18 -0600

    Implementación PriorityQueue

--- Beta0.1/main.java
+++ Beta0.1/main.java
@@ -1,5 +1,29 @@
+import java.util.PriorityQueue;
+
 class main{
 	public static void main(String[] args) {
 		System.out.println("Bienvenido a PriorityHealth");
+
+		/*----------------
+		| Priority Queue |
+		----------------*/
+		PriorityQueue pq = new PriorityQueue();
+
+		//Pacientes de prueba
+		Paciente pacienteuno = new Paciente("Diego Morales", 3);
+		Paciente pacientedos = new Paciente("Juan Benitez", 1);
+		Paciente pacientetres = new Paciente("Julio Gonzalez", 4);
+		Paciente pacientecuatro = new Paciente("Michel Ramirez", 2);
+
+		//Agregar pacientes a la Priority Queue
+		pq.add(pacienteuno);
+		pq.add(pacientedos);
+		pq.add(pacientetres);
+		pq.add(pacientecuatro);
+
+		System.out.println(pq.poll());
+		System.out.println(pq.poll());
+		System.out.println(pq.poll());
+		System.out.println(pq.poll());
 	}
 }
